3eb1b4396fd6e7a6dd3f68397878a3de1ca466f6
Zyxkon <92172151+Zyxkon@users.noreply.github.com>
2022-03-19 22:05:59 +0700

    Update quick_sort.pas

--- quick_sort.pas
+++ quick_sort.pas
@@ -2,10 +2,10 @@ unit quicksort;
 interface
 type
     IntArray = array of Integer;
-procedure qsort(var arr: array of Integer; low: Integer; high: Integer);
 procedure print_array(arr: array of Integer);
 function contains(arr: array of Integer; element: Integer):Boolean;
 function randomize_array(var arr: array of Integer) : IntArray;
+function qsort(arr: array of Integer) : IntArray;
 implementation
 procedure exchange(var arr: array of Integer; i: Integer; j: Integer);
     var
@@ -32,15 +32,15 @@ function partition(var arr: array of Integer; low: Integer; high: Integer) : Int
     exchange(arr, i+1, high);
     partition:=i+1;
     end;
-procedure qsort(var arr: array of Integer; low: Integer; high: Integer);
+procedure qsort_(var arr: array of Integer; low: Integer; high: Integer);
     var
         pivot: Integer;
     begin
     if (high > low) then
         begin
             pivot := partition(arr, low, high);
-            qsort(arr, low, pivot-1);
-            qsort(arr, pivot+1, high);
+            qsort_(arr, low, pivot-1);
+            qsort_(arr, pivot+1, high);
         end;
     end;
 procedure print_array(arr: array of Integer);
@@ -80,7 +80,7 @@ function randomize_array(var arr: array of Integer) : IntArray;
         while (Length(randommed) < Length(arr)) do 
         begin
         randint := arr[random(Length(arr))];
-        if not quicksort.contains(found, randint) then
+        if not contains(found, randint) then
             begin
                 Insert(randint, found, Length(found));
                 Insert(randint, randommed, Length(randommed));
@@ -88,5 +88,24 @@ function randomize_array(var arr: array of Integer) : IntArray;
         end;    
         randomize_array := randommed;
     end;
-
+function copy_array(arr: array of Integer) : IntArray;
+    var
+        i: Integer;
+        result: array of Integer;
+    begin
+    SetLength(result, 0);
+    for i:=0 to Length(arr) - 1 do
+    begin
+        Insert(arr[i], result, Length(result));
+    end;
+    copy_array := result;
+    end;
+function qsort(arr: array of Integer) : IntArray;
+    var
+        result: IntArray;
+    begin
+        result := copy_array(arr);
+        qsort_(result, 0, Length(result)-1);
+        qsort := result;
+end;
 end.
